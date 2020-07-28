0b0f9434847344040f5d4213b8fb6d173823d13b
MonicaLuca <monica.lucarini28@gmail.com>
2020-07-28 13:23:57 -0400

    timer1.js added

---
+++ timer1.js
@@ -0,0 +1,13 @@
+const args = process.argv.slice(2);
+const sorted = args.sort(function(a, b) {
+  return a - b;
+});
+
+for (let time of sorted) {
+  if (time < 0 || time === '' || isNaN(time) === true) {
+    continue;
+  } else {
+    setTimeout(() => process.stdout.write('beep'), time * 1000);
+  }
+}
+
